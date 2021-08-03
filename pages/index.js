77856becffbc0859b1c5a0e2baa2464f9097aa39
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-08-03 15:38:46 +0800

    Initial commit of basic baseball nextjs website

---
+++ pages/index.js
@@ -0,0 +1,140 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+import getEndpoint from '../lib/endpoints.js'
+import useSWR from 'swr'
+
+export default function Home() {
+  const { data, error } = useSWR('test', getData)
+  let dataElement;
+  if (error) {
+    dataElement = <p>Error</p>
+  } else if (!data) {
+    dataElement = <p>Loading...</p>
+  } else if (data) {
+    // dataElement = <pre>{JSON.stringify(data[0], null, 2)}</pre>
+    dataElement = (
+      <div className={styles.games}>
+        {data.map(game => {
+          let runnerDict = game.liveData.linescore.offense
+          let runners = []
+          for (const base in ["first", "second", "third"]) {
+            if (base in runnerDict) {
+              runners.append(base)
+          }
+
+          let gameInfo;
+          let balls = game.liveData.linescore.balls
+          let strikes = game.liveData.linescore.strikes
+          let outs = game.liveData.linescore.outs
+
+          if (game.gameData.status.statusCode.toLowerCase() !== 'f' && game.gameData.status.statusCode.toLowerCase() !== 's' && game.gameData.status.statusCode.toLowerCase() !== 'p') {
+            gameInfo = (
+              <div>
+                <p>Current inning: {game.liveData.linescore.inningHalf} {game.liveData.linescore.currentInningOrdinal}</p>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="78.03606468133466 83.53300942673016 496.3416517857596 366.3416517857596" width="50" height="50">
+                  <defs>
+                    <path d="M325.21 84.53L441.38 200.7L325.21 316.87L209.04 200.7L325.21 84.53Z" id="b2YCI6YlRZ"></path>
+                    <path d="M195.21 214.53L311.38 330.7L195.21 446.87L79.04 330.7L195.21 214.53Z" id="avI05k38w"></path>
+                    <path d="M455.21 214.53L571.38 330.7L455.21 446.87L339.04 330.7L455.21 214.53Z" id="cpNV2Weba"></path>
+                  </defs>
+                  <g>
+                    <g>
+                      <use xlinkHref="#b2YCI6YlRZ" opacity="1" fill={runners.includes("second") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
+                    </g>
+                    <g>
+                      <use xlinkHref="#avI05k38w" opacity="1" fill={runners.includes("third") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
+                    </g>
+                    <g>
+                      <use xlinkHref="#cpNV2Weba" opacity="1" fill={runners.includes("first") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
+                    </g>
+                  </g>
+                </svg>
+                <p>{balls}-{strikes}, {outs} out</p>
+              </div>
+            )
+          }
+          return (
+          <li className={styles.card}>
+            <p>Status: {game.gameData.status.detailedState}</p>
+            <table className={styles.score}>
+              <tr>
+                <td>{game.gameData.teams.away.name}</td>
+                <td>{game.liveData.linescore.teams.away.runs}</td>
+              </tr>
+              <tr>
+                <td>{game.gameData.teams.home.name}</td>
+                <td>{game.liveData.linescore.teams.home.runs}</td>
+              </tr>
+            </table>
+            {gameInfo}
+            {/* <p>Balls: {game.liveData.linescore.balls}</p>
+            <div>
+              <svg height="22" width="22">
+                <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 1 && balls !== 4 ? 'yellow' : 'transparent'} />
+              </svg>
+              <svg height="22" width="22">
+                <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 2 && balls !== 4 ? 'yellow' : 'transparent'} />
+              </svg>
+              <svg height="22" width="22">
+                <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 3 && balls !== 4 ? 'yellow' : 'transparent'} />
+              </svg>
+            </div>
+            <p>Strikes: {game.liveData.linescore.strikes}</p>
+            <div>
+              <svg height="22" width="22">
+                <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={strikes >= 1 && strikes !== 3 ? 'red' : 'transparent'} />
+              </svg>
+              <svg height="22" width="22">
+                <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={strikes >= 2 && strikes !== 3 ? 'red' : 'transparent'} />
+              </svg>
+            </div>
+            <p>Outs: {game.liveData.linescore.outs}</p> */}
+            {/* { runners.length ? <p>Runners on: {runners}</p> : <p>No runners on</p> } */}
+          </li>
+      )}})}
+      </div>
+    )
+  }
+  return (
+    // <div className={styles.container}>
+    <div>
+      <Head>
+        <title>Latest MLB scores</title>
+        <meta name="description" content="MLB website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className={styles.heading}>
+        Games
+      </h1>
+      {dataElement}
+    </div>
+  )
+}
+
+async function getData() {
+  let data;
+  await fetch(getEndpoint('schedule') + new URLSearchParams({ sportId: 1, date: '2021-08-01' })).then(
+    res => res.json()
+  ).then(
+    json => data = json
+  )
+
+  let todayGamePks = []
+  data.dates[0].games.forEach(game => {
+    todayGamePks.push(game.gamePk)
+  })
+
+  let games = []
+  for (const gamePk of todayGamePks) {
+    let gameData;
+    await fetch(getEndpoint('game', { gamePk, gamePk })).then(
+      res => res.json()
+    ).then(
+      json => gameData = json
+    )
+    games.push(gameData)
+  }
+
+  return games
+}
