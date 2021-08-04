e595cf0a4c62139ab9250b0d94e6712aff3b6d5a
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-08-04 22:38:36 +0800

    Put games widget in component, finished play by play, put in boxscore div

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
+import GameSmall from '../components/game_small'
 import styles from '../styles/Home.module.scss'
 import getEndpoint from '../lib/endpoints.js'
 import useSWR from 'swr'
 
 export default function Home() {
-  const { data, error } = useSWR('test', getData)
+  const { data, error } = useSWR('test', getData, { refreshInterval: 1 })
   let dataElement;
   if (error) {
     console.log(error)
@@ -15,62 +16,7 @@ export default function Home() {
   } else if (data) {
     dataElement = (
       <div className={styles.games}>
-        {data.map(game => {
-          let runnerDict = game.liveData.linescore.offense
-          let runners = []
-          for (const base in ["first", "second", "third"]) {
-            if (base in runnerDict) {
-              runners.append(base)
-          }
-
-          let gameInfo;
-          let balls = game.liveData.linescore.balls
-          let strikes = game.liveData.linescore.strikes
-          let outs = game.liveData.linescore.outs
-          
-          // If the game is currently ongoing then have current game stuff
-          if (game.gameData.status.statusCode.toLowerCase() !== 'f' && game.gameData.status.statusCode.toLowerCase() !== 's' && game.gameData.status.statusCode.toLowerCase() !== 'p') {
-            gameInfo = (
-              <div>
-                <p>Current inning: {game.liveData.linescore.inningHalf} {game.liveData.linescore.currentInningOrdinal}</p>
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="78.03606468133466 83.53300942673016 496.3416517857596 366.3416517857596" width="50" height="50">
-                  <defs>
-                    <path d="M325.21 84.53L441.38 200.7L325.21 316.87L209.04 200.7L325.21 84.53Z" id="b2YCI6YlRZ"></path>
-                    <path d="M195.21 214.53L311.38 330.7L195.21 446.87L79.04 330.7L195.21 214.53Z" id="avI05k38w"></path>
-                    <path d="M455.21 214.53L571.38 330.7L455.21 446.87L339.04 330.7L455.21 214.53Z" id="cpNV2Weba"></path>
-                  </defs>
-                  <g>
-                    <g>
-                      <use xlinkHref="#b2YCI6YlRZ" opacity="1" fill={runners.includes("second") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
-                    </g>
-                    <g>
-                      <use xlinkHref="#avI05k38w" opacity="1" fill={runners.includes("third") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
-                    </g>
-                    <g>
-                      <use xlinkHref="#cpNV2Weba" opacity="1" fill={runners.includes("first") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
-                    </g>
-                  </g>
-                </svg>
-                <p>{balls}-{strikes}, {outs} out</p>
-              </div>
-            )
-          }
-          return (
-          <li className={styles.card}>
-            <p>Status: {game.gameData.status.detailedState}</p>
-            <table className={styles.score}>
-              <tr>
-                <td>{game.gameData.teams.away.name}</td>
-                <td>{game.liveData.linescore.teams.away.runs}</td>
-              </tr>
-              <tr>
-                <td>{game.gameData.teams.home.name}</td>
-                <td>{game.liveData.linescore.teams.home.runs}</td>
-              </tr>
-            </table>
-            {gameInfo}
-          </li>
-      )}})}
+        {data.map(game => <GameSmall key={`gwidget_small_${game.gameData.game.pk}`} game={game} />)}
       </div>
     )
   }
@@ -107,5 +53,27 @@ async function getData() {
     responses => Promise.all(responses.map(async (res) => await res.json()))
   )
 
-  return games
+  // games.push(games[Math.floor(Math.random() * choices.length)])
+
+  return games.sort((a, b) => {
+    switch ((a.gameData.status.statusCode, b.gameData.status.statusCode)) {
+      case ('F', 'S'):
+        return -1
+      case ('S', 'F'):
+        return 1
+      case ('F', 'PW'):
+        return -1
+      case ('PW', 'F'):
+        return 1
+      case ('P', 'F'):
+        return 1
+      case ('F', 'P'):
+        return -1
+      case ('S', 'P'):
+        return 1
+      case ('P', 'S'):
+        return -1
+    }
+    return 0
+  })
 }
