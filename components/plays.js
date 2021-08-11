a416c8c96dc2e2f03576bb9219bfb8dd0d029a60
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-08-11 14:42:44 +0800

    Added runners to play

--- components/plays.js
+++ components/plays.js
@@ -15,10 +15,22 @@ export default function Plays({ game, setTimestamp }) {
       ? int + ordinals[digits[0] - 1]
       : int + ordinals[3];
   };
+  let gameOld = game;
   game = game.liveData.plays
   let balls, strikes, outs;
   let plays = [game.currentPlay];
   plays.push(...game.allPlays)
+  let runnerDict = gameOld.liveData.linescore.offense
+  let runners = []
+  for (const base of ["first", "second", "third"]) {
+    if (base in runnerDict) {
+      runners.push(base)
+    }
+  }
+
+  if (plays[1].about.inning < plays[plays.length -1].about.inning) {
+    plays = [plays[0], ...plays.slice(1).reverse()]
+  }
   return (
     plays.map((play, index) => {
       balls = play.count.balls
@@ -38,37 +50,56 @@ export default function Plays({ game, setTimestamp }) {
           interval = setInterval(() => content.style.maxHeight = content.scrollHeight + "px", 100);
         }
       }
-
       return (
         <div onClick={collapseListener} className={styles.cardLight} key={`play_${index}`}>
         <div>
           <p>{play.about.halfInning.charAt(0).toUpperCase() + play.about.halfInning.slice(1)} of {toOrdinalSuffix(play.about.inning)}{play.result.description && ` — ${play.result.description}`}</p>
           <div>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 1 && balls !== 4 ? 'yellow' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 1 ? 'yellow' : 'transparent'} />
             </svg>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 2 && balls !== 4 ? 'yellow' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 2 ? 'yellow' : 'transparent'} />
             </svg>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 3 && balls !== 4 ? 'yellow' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={balls >= 3 ? 'yellow' : 'transparent'} />
             </svg>
           </div>
           <div>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={strikes >= 1 && strikes !== 3 ? 'orange' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={strikes >= 1 ? 'orange' : 'transparent'} />
             </svg>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={strikes >= 2 && strikes !== 3 ? 'orange' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={strikes >= 2 ? 'orange' : 'transparent'} />
             </svg>
           </div>
           <div>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={outs >= 1 && outs !== 3 ? 'red' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={outs >= 1 ? 'red' : 'transparent'} />
             </svg>
             <svg height="22" width="22">
-              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={outs >= 2 && outs !== 3 ? 'red' : 'transparent'} />
+              <circle cx="11" cy="11" r="10" stroke-width="1" stroke="black" fill={outs >= 2 ? 'red' : 'transparent'} />
             </svg>
+          </div>
+          <div>
+            {play === game.currentPlay && (<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="78.03606468133466 83.53300942673016 496.3416517857596 366.3416517857596" width="50" height="50">
+              <defs>
+                <path d="M325.21 84.53L441.38 200.7L325.21 316.87L209.04 200.7L325.21 84.53Z" id="b2YCI6YlRZ"></path>
+                <path d="M195.21 214.53L311.38 330.7L195.21 446.87L79.04 330.7L195.21 214.53Z" id="avI05k38w"></path>
+                <path d="M455.21 214.53L571.38 330.7L455.21 446.87L339.04 330.7L455.21 214.53Z" id="cpNV2Weba"></path>
+              </defs>
+              <g>
+                <g>
+                  <use xlinkHref="#b2YCI6YlRZ" opacity="1" fill={runners.includes("second") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
+                </g>
+                <g>
+                  <use xlinkHref="#avI05k38w" opacity="1" fill={runners.includes("third") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
+                </g>
+                <g>
+                  <use xlinkHref="#cpNV2Weba" opacity="1" fill={runners.includes("first") ? 'yellow' : 'lightgrey'} fill-opacity="1"></use>
+                </g>
+              </g>
+            </svg>)}
           </div>
         </div>
         <div id={`pitches_${index}`} className={styles.pitches}>
@@ -81,6 +112,7 @@ export default function Plays({ game, setTimestamp }) {
             )
           }) : null}
         </div>
+        {play === plays.currentPlay && collapseListener()}
         </div>
       )
     })
